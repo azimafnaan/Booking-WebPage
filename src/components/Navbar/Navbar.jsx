50ea21b17f07c3eb53d31fbc965fd71b942666d3
azimafnaan <azim.afnaan@hotmail.com>
2022-08-30 03:57:33 +0600

    Hello World

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <div>
+            <p>navbar</p>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/hotelList/HotelList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HotelList.css';
+const HotelList = () => {
+    return (
+        <div>
+            <p>Hotel List</p>
+        </div>
+    );
+};
+
+export default HotelList;
