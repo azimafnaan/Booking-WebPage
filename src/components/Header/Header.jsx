2571ed96ecf9802459c96663e975fa51166f55d0
azimafnaan <azim.afnaan@hotmail.com>
2022-08-30 14:00:56 +0600

    Header Section Design Complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { faAtom, faBed, faCar, faPlane, faRestroom, faTaxi } from '@fortawesome/free-solid-svg-icons';
+import { faAtom, faBed, faCalendarDays, faCar, faPerson, faPlane, faRestroom, faTaxi } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Header.css';
@@ -30,6 +30,23 @@ const Header = () => {
                 </div>
                 <h1 className="headerTitle">Find your next stay</h1>
                 <p className="headerDesc">Search deals on hotels, homes, and much more...</p>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="inputIcon" />
+                        <input type="text" placeholder="Where are you going?" className="headerSearchInput"/>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="inputIcon" />
+                       <span className="headerSearchText">Check-in - Check-out</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="inputIcon" />
+                       <span className="headerSearchText">2 adults - 2 children - 1 room</span>
+                    </div>
+                    <div className="headerSearchItem">
+                       <button className="headerBtn">Search</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
